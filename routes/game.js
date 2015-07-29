6e3b6319aae6483df0a0e5fd81e51f74d3d0d926
dipwood <saito.tenjirou@gmail.com>
2015-07-29 16:31:48 -0400

    started angular testing

--- routes/game.js
+++ routes/game.js
@@ -4,13 +4,13 @@ var router = express.Router();
 // GET handler for the game.
 router.get('/', function(req, res, next) 
 	{
-	res.render('game', { title: 'Game' , username: req.session.username})
+	res.render('game', { title: 'SC2 Manager' , username: req.session.username})
 	})
 
 // POST handler for the game. currently unused
 router.post('/game', function(req, res, next) 
 	{
-	res.render('game', { title: 'Game' , username: req.session.username})
+	res.render('game', { title: 'SC2 Manager' , username: req.session.username})
 	})
 
 module.exports = router;
